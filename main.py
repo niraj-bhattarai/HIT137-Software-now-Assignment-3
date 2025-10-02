dcf4cf03d317ab97a4309380d7b4e464c5cdb489
Bishal Raut <bishal.raut@axiossoftwork.com>
2025-10-03 09:50:42 +1000

    updted:commetns for main.py

--- main.py
+++ main.py
@@ -3,9 +3,18 @@
 
 #Detailed commenting required
 
+
+
+#importing the pythgui from AIApp
 from pythgui import AIApp
 
 
+
+
+#Main function for the program
 if __name__ == "__main__":
+    #Creating AIAPP instance
     app=AIApp()
+
+    #main to keep the window open 
     app.mainloop() 
